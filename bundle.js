8dd19ce3d41903320a7f1f963542724fbfafa287
nobalitscouncil <nobalitsc@gmail.com>
2025-05-18 15:10:36 +0530

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,3 @@
+
+document.getElementById("root").innerHTML = `<h1>Nobalits Data Project</h1>
+<p>Connected to backend: <a href='https://backendc-ppul.onrender.com' target='_blank'>Click to open backend</a></p>`;
